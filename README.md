ca3e78b6bf7ddf111aeaf76a7da69835ed163733
andriyanegorov <87094689+andriyanegorov@users.noreply.github.com>
2026-04-14 00:12:29 +0300

    da

--- README.md
+++
@@ -1,1 +0,0 @@
-# brsimulator
--- copy_html.ps1
+++
@@ -1,4 +0,0 @@
-$src = Join-Path $env:USERPROFILE "AppData\Roaming\Code\User\History\113bcab8\eMBM.html"
-$dst = Join-Path (Get-Location) "index.html"
-Copy-Item -Path $src -Destination $dst -Force
-Write-Output "Copied $src to $dst"
